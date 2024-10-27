e2035d17b13b989c8972232c8313ec4422048c57
unknown <panevgjorgi@gmail.com>
2024-10-27 23:57:51 +0100

    Initial commit

--- tests/day5/environments.spec.ts
+++ tests/day5/environments.spec.ts
@@ -1,6 +1,7 @@
-import { test } from '@playwright/test';
+/*import { test } from '@playwright/test';
 
 test('Working with environments', async ({  }) => {
     console.log(process.env.URL);
     console.log(process.env.USER);
 });
+*/
